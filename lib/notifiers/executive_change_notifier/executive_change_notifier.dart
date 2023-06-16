3815f05d58856b7bd780845092e4ea220c2b9d57
Soomali <mehmetaliguunaydin@gmail.com>
2023-06-16 19:19:53 +0300

    written skeleton.

---
+++ lib/notifiers/executive_change_notifier/executive_change_notifier.dart
@@ -0,0 +1,4 @@
+import 'package:smart_tent_city_app/model/error_wrapper.dart';
+import 'package:smart_tent_city_app/notifiers/async_change_notifier.dart';
+
+class ExecutiveChangeNotifier extends AsyncChangeNotifier with ErrorWrapper {}
