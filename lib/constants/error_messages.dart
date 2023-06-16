681572ca3e2cc80508ca07ff0b4c6795be2931b3
Soomali <mehmetaliguunaydin@gmail.com>
2023-06-16 19:59:26 +0300

    added executive change notifier.

--- lib/constants/error_messages.dart
+++ lib/constants/error_messages.dart
@@ -1,4 +1,6 @@
 class ErrorMessageConstants {
   static const String emailAuthError = 'email veya şifre hatalı';
   static const String phoneAuthError = 'telefon doğrulaması başarısız!';
+  static const String executiveGetError =
+      'Bilgilere alınırken bir hata ile karşılaşıldı.';
 }
--- lib/notifiers/executive_change_notifier/executive_change_notifier.dart
+++ lib/notifiers/executive_change_notifier/executive_change_notifier.dart
@@ -1,4 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:smart_tent_city_app/constants/error_ids.dart';
+import 'package:smart_tent_city_app/constants/error_messages.dart';
+import 'package:smart_tent_city_app/constants/firebase.dart';
+import 'package:smart_tent_city_app/model/ExecutiveModel.dart';
 import 'package:smart_tent_city_app/model/error_wrapper.dart';
 import 'package:smart_tent_city_app/notifiers/async_change_notifier.dart';
+import 'package:smart_tent_city_app/pages/provider/data_change_notifier.dart';
 
-class ExecutiveChangeNotifier extends AsyncChangeNotifier with ErrorWrapper {}
+class ExecutiveChangeNotifier extends DataChangeNotifier<ExecutiveModel>
+    with ErrorWrapper {
+  Future<void> _get(String id) async {
+    final doc = await FirebaseFirestore.instance
+        .collection(executiveCollectionPath)
+        .doc(id)
+        .get();
+    this.data = ExecutiveModel.fromJson(doc.data() as Map<String, dynamic>);
+  }
+
+  void get(String id) => wrapAsync(
+      () => _get(id),
+      ErrorIdConstants.executiveGetErrorId,
+      ErrorMessageConstants.executiveGetError);
+}
