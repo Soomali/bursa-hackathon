7e1a399a377c7b5bd038f59dfec448c9bb4b7b53
Soomali <mehmetaliguunaydin@gmail.com>
2023-06-16 20:10:40 +0300

    added victim change notifier.

--- lib/constants/error_ids.dart
+++ lib/constants/error_ids.dart
@@ -2,4 +2,7 @@ class ErrorIdConstants {
   static const String emailAuthErrorId = 'emailAuthErrorId';
   static const String phoneAuthErrorId = 'phoneAuthErrorId';
   static const String executiveGetErrorId = 'executiveGetErrorId';
+  static const String victimGetErrorId = 'victimGetErrorId';
+  static const String victimPostErrorId = 'victimPostErrorId';
+  static const String victimDeleteErrorId = 'victimDeleteErrorId';
 }
