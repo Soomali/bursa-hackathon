f512ba7136d363a50494dafb85b985561b9bdca7
Soomali <mehmetaliguunaydin@gmail.com>
2023-06-16 20:48:33 +0300

    imported missing imports.

--- lib/pages/provider/provider_state.dart
+++ lib/pages/provider/provider_state.dart
@@ -1,9 +1,10 @@
 import 'package:smart_tent_city_app/notifiers/async_change_notifier.dart';
 
 import '../../model/error_model.dart';
+import '../../notifiers/async_change_notifier_state.dart';
 
-class ProviderState<T>{
-  List<ErrorModel> errors=[];
+class ProviderState<T> {
+  List<ErrorModel> errors = [];
   AsyncChangeNotifierState state;
   T? data;
 
