5b9f57286e26e251dba4a36ca30008d929e250a2
bbece <bbeceok@hotmail.com>
2023-06-17 07:52:07 +0300

    victim_data profil detayları oluşturuldu

--- lib/pages/victim_data/victim_data.dart
+++ lib/pages/victim_data/victim_data.dart
@@ -11,11 +11,33 @@ class VictimData extends StatelessWidget {
     final victim =
         Provider.of<VictimChangeNotifier>(context, listen: false).data!;
 
+
+
+
     return BackgroundPage(
         child: SizedBox(
       width: double.infinity,
       child: Column(
-        children: [],
+        children: [
+        Text(victim.id),
+          Divider(color: Colors.red,),
+          Text(victim.name),
+          Divider(color: Colors.red,),
+          Text(victim.surname),
+          Divider(color: Colors.red,),
+          victim.birthday != null ? Text("${victim.birthday}"): Text("-"),
+          Divider(color: Colors.red,),
+          victim.blood_type != null ? Text("${victim.blood_type}"): Text("-"),
+          Divider(color: Colors.red,),
+          victim.phone_number != null ? Text("${victim.phone_number}"): Text("-"),
+          Divider(color: Colors.red,),
+          victim.tent_number != null ? Text("${victim.tent_number}"): Text("-"),
+          Divider(color: Colors.red,),
+          victim.additional_data != null ? Text("${victim.additional_data}"):Text("-"),
+          Divider(color: Colors.red,),]
+
+
+    ],
       ),
     ));
   }
