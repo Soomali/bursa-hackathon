5f61d4c650b63e94b4d3cbd32e31c01afe4acc97
Soomali <mehmetaliguunaydin@gmail.com>
2023-06-17 12:10:20 +0300

    added list requests.

--- lib/pages/tent_city_requests/tent_city_requests_body.dart
+++ lib/pages/tent_city_requests/tent_city_requests_body.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:smart_tent_city_app/notifiers/pagination_change_notifier/requests_list_change_notifier.dart';
+import 'package:smart_tent_city_app/pages/background_page.dart';
 
 class TentCityRequests extends StatelessWidget {
   const TentCityRequests({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final notifier = RequestsListChangeNotifier();
+
+    return ChangeNotifierProvider.value(
+      value: notifier,
+      child: BackgroundPage(child: TentCityRequests()),
+    );
   }
 }
