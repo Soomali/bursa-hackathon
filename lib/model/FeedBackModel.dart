d376afef1a3a50e0cf7b358af63a87e72c61b1a1
Soomali <mehmetaliguunaydin@gmail.com>
2023-06-17 05:43:25 +0300

    added feedback change notifier.

--- lib/model/FeedBackModel.dart
+++ lib/model/FeedBackModel.dart
@@ -1,21 +1,30 @@
+enum FeedbackStatus { seen, notSeen }
+
 class FeedBackModel {
   String topic;
   String description;
-  FeedBackModel({required this.topic, required this.description});
-  factory FeedBackModel.fromJson(Map<String, dynamic> json) {
+  String id;
+  FeedbackStatus status;
+  FeedBackModel(
+      {required this.topic,
+      required this.id,
+      required this.status,
+      required this.description});
+  factory FeedBackModel.fromJson(Map<String, dynamic> json, String id) {
     final topic = json['topic'];
     final description = json['description'];
 
     return FeedBackModel(
       topic: json['topic'],
+      id: id,
+      status: json['status'] == FeedbackStatus.seen.name
+          ? FeedbackStatus.seen
+          : FeedbackStatus.notSeen,
       description: json['description'],
     );
   }
 
   Map<String, dynamic> toJson() {
-    return {
-      'topic': topic,
-      'description': description,
-    };
+    return {'topic': topic, 'description': description, 'status': status.name};
   }
 }
