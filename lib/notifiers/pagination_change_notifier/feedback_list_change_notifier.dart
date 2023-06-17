98ec23e52a0ee997529bc51f13bad85ad1e45f5d
Soomali <mehmetaliguunaydin@gmail.com>
2023-06-17 06:32:00 +0300

    bug fixes made.

--- lib/notifiers/pagination_change_notifier/feedback_list_change_notifier.dart
+++ lib/notifiers/pagination_change_notifier/feedback_list_change_notifier.dart
@@ -2,20 +2,21 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:smart_tent_city_app/constants/error_ids.dart';
 import 'package:smart_tent_city_app/constants/error_messages.dart';
 import 'package:smart_tent_city_app/constants/firebase.dart';
+import 'package:smart_tent_city_app/model/FeedBackModel.dart';
 import 'package:smart_tent_city_app/model/RequestModel.dart';
 import 'package:smart_tent_city_app/notifiers/pagination_change_notifier/pagination_change_notifier.dart';
 
 class FeedbackListChangeNotifier extends PaginationChangeNotier {
   Future<void> _get(String? tentCityId) async {
     final query = FirebaseFirestore.instance
-        .collection(requestCollectionPath)
+        .collection(feedbackCollectionPath)
         .limit(10)
         .where('tentCityId', isEqualTo: tentCityId);
 
     final snapshot = await paginate(query).get();
     final newData = snapshot.docs
         .map((e) =>
-            RequestModel.fromJson(e.data() as Map<String, dynamic>, e.id))
+            FeedBackModel.fromJson(e.data() as Map<String, dynamic>, e.id))
         .toList();
     this.data = [...(this.data ?? []), ...newData];
     notifyListeners();
