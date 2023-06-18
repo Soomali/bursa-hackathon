07a155dbb4c5ff1e070b134a87232504fee24e3b
Soomali <mehmetaliguunaydin@gmail.com>
2023-06-18 05:11:26 +0300

    fixed things.

--- lib/pages/complaint/complaint_body.dart
+++ lib/pages/complaint/complaint_body.dart
@@ -19,43 +19,67 @@ class _ComplaintBodyState extends State<ComplaintBody> {
   String description = '';
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          '*lütfen görevlilerimizin elinden geleni yaptığını, istek ve şikayetinizi iletirken dikkate alınız*',
-          textAlign: TextAlign.center,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * .2,
+            ),
+            Text(
+              'lütfen görevlilerimizin elinden geleni yaptığını istek ve şikayetinizi iletirken dikkate alınız',
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  hintText: 'Konu', border: OutlineInputBorder()),
+              onChanged: (val) {
+                setState(() {
+                  topic = val;
+                });
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              maxLines: null,
+              decoration: InputDecoration(
+                  hintText: 'Detay', border: OutlineInputBorder()),
+              onChanged: (val) {
+                setState(() {
+                  topic = val;
+                });
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ComplaintButton(onPressed: () {
+              final victimModel =
+                  Provider.of<VictimChangeNotifier>(context, listen: false)
+                      .data!;
+              final FeedBackModel model = FeedBackModel(
+                  topic: topic,
+                  victimId: victimModel.id,
+                  tentCityId: victimModel.tentCityId,
+                  id: FirebaseFirestore.instance
+                      .collection(feedbackCollectionPath)
+                      .doc()
+                      .id,
+                  status: FeedbackStatus.notSeen,
+                  description: description);
+              Provider.of<FeedbackChangeNotifier>(context, listen: false)
+                  .create(model);
+            })
+          ],
         ),
-        TextField(
-          onChanged: (val) {
-            setState(() {
-              topic = val;
-            });
-          },
-        ),
-        TextField(
-          onChanged: (val) {
-            setState(() {
-              description = val;
-            });
-          },
-        ),
-        ComplaintButton(onPressed: () {
-          final victimModel =
-              Provider.of<VictimChangeNotifier>(context, listen: false).data!;
-          final FeedBackModel model = FeedBackModel(
-              topic: topic,
-              victimId: victimModel.id,
-              tentCityId: victimModel.tentCityId,
-              id: FirebaseFirestore.instance
-                  .collection(feedbackCollectionPath)
-                  .doc()
-                  .id,
-              status: FeedbackStatus.notSeen,
-              description: description);
-          Provider.of<FeedbackChangeNotifier>(context, listen: false)
-              .create(model);
-        })
-      ],
+      ),
     );
   }
 }
